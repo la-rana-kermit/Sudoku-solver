31ae0e82c13bab6224b0ca53d71b8adb1968a177
N36L3C7 <31883347+n36l3c7@users.noreply.github.com>
2023-08-25 18:02:56 +0200

    Feat: Add exist_in_column method
    Added exist_in_column method:
    Methods to check if a number exists in column value list

--- column.py
+++ column.py
@@ -3,9 +3,9 @@ class Column(object):
     Class that represent single sudoku column
     """
 
-    column = None
+    column: list = []
 
-    def __init__(self, column):
+    def __init__(self, column: list):
         """
         Class constructor that represent single sudoku column
 
@@ -14,3 +14,12 @@ class Column(object):
         :type column: int
         """
         self.column = column
+
+    def exist_in_column(self, number: int):
+        """
+        Methods to check if a number exists in column value list
+
+        :param number: Number to check
+        :type number: int
+        """
+        return True if number in self.column else False
